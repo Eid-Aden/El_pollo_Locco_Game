7c671c5901e63af676798d3122695ce5296d5be5
Eid Aden <e-aden@hotmail.de>
2025-04-07 11:09:54 +0200

    comimit

--- modules/classes.world.js
+++ modules/classes.world.js
@@ -18,10 +18,10 @@ class World {
     new Background('img/5_background/layers/2_second_layer/2.png', 719.5),
     new Background('/img/5_background/layers/1_first_layer/2.png', 719.5),
 
-    new Background('/img/5_background/layers/air.png', 719),
-    new Background('img/5_background/layers/3_third_layer/2.png', 719),
-    new Background('img/5_background/layers/2_second_layer/2.png', 719),
-    new Background('/img/5_background/layers/1_first_layer/2.png', 719),
+    new Background('/img/5_background/layers/air.png', 719 * 4),
+    new Background('img/5_background/layers/3_third_layer/2.png', 719 * 4),
+    new Background('img/5_background/layers/2_second_layer/2.png', 719 * 4),
+    new Background('/img/5_background/layers/1_first_layer/2.png', 719 * 4),
   ];
 
   ctx;
